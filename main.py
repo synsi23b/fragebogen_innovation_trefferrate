05ceb13ea0da909fc0c8601143594e773feb9a98
synsi <mail@himitsu.dev>
2023-08-18 09:45:10 +0900

    finished

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from pathlib import Path
-import openpyxl
+import pandas
+from helper import correct_inc, correct_rad
 
 FOLDER = Path(__file__).resolve().parent
 INFILE = FOLDER / "w3_700-22.xlsx"
 OUTFILE = FOLDER / "result.xlsx"
 
-inwb = openpyxl.load_workbook(INFILE)
-inws = inwb.active
 
-
-print(inws["A1:ABX1"])
-
+interview = "Interview-Nummer (fortlaufend)"
+condition = "Fragebogen, der im Interview verwendet wurde"
 fieldnames = [
     "Field_{}: Ausweichoption (negativ) oder Anzahl ausgewählter Optionen",
-    "Field_{}: Tiere &amp; Biologie",
+    "Field_{}: Tiere & Biologie",
     "Field_{}: Technologie",
     "Field_{}: Kleidung",
     "Field_{}: Energie",
@@ -29,3 +27,45 @@ fieldnames = [
     "Field_{}: andere"
     ]
 
+def calc_hitrate(df, index, fieldnum, correct):
+    try:
+        field = fieldnames[0].format(fieldnum)
+        select_count = int(df[field][index])
+        correct = correct[fieldnum]
+        select = []
+        for idx, field in enumerate(fieldnames[1:], 1):
+            fname = field.format(fieldnum)
+            if df[fname][index] == 2:
+                select.append(idx)
+        cor_count = 0
+        for sel in select:
+            if sel in correct:
+                cor_count += 1
+        return float(cor_count / select_count)
+    except ValueError:
+        #return None
+        return "#NULL!"
+
+df = pandas.read_excel(INFILE)
+print(df.columns)
+
+result = []
+for ind in df.index:
+    inr = df[interview][ind]
+    cnd = df[condition][ind]
+    if cnd == "I":
+        correct = correct_inc
+    elif cnd == "R":
+        correct = correct_rad
+    else:
+        raise ValueError("Unknown condition")
+    data = [inr]
+    for i in range(1, 51):
+        data.append(calc_hitrate(df, ind, i, correct))
+        if type(data[-1]) is float:
+            print(f"Interview {inr} Field {i:02} Hitrate: {data[-1]}")
+    result.append(data)
+
+colnames = [interview] + [f"hitrate_{i}" for i in range(1, 51)]
+outframe = pandas.DataFrame(result, columns=colnames)
+outframe.to_excel(OUTFILE, index=False)
